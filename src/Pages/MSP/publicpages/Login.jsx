7c8a0918736c88c30cf69b32b23fba661637f0a6
prashantkhamitkar <prashant@ashunya.com>
2024-07-27 17:15:28 +0530

    some changes done

--- src/Pages/MSP/publicpages/Login.jsx
+++ src/Pages/MSP/publicpages/Login.jsx
@@ -115,7 +115,7 @@ if(loading){
               src="/ashunyalogo.png"
               style={{ width: "6rem" }}
               className="mb-3"
-              loading="eager"
+              loading="lazy"
               alt="ashunya logo"
             />
             <h1 className="h4 h-md-3 h-lg-2 font-weight-bold">
@@ -149,7 +149,7 @@ if(loading){
               style={{ width: "6rem" }}
               className="mb-3"
               alt="ashunya logo"
-              loading="eager"
+              loading="lazy"
             />
             <h1 className="h4 h-md-3 h-lg-2 font-weight-bolder">
               Sign In To Helpdesk Portal
